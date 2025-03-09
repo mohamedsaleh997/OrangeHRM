16b9cb9a12f7a06c36c34a3e5ea72411ef2a9b49
saleh <m.salehh997@gmail.com>
2025-03-09 13:06:07 +0200

    Latest Commit

--- src/main/java/com/testwebsite/drivers/BrowserFactory.java
+++ src/main/java/com/testwebsite/drivers/BrowserFactory.java
@@ -4,6 +4,8 @@ import com.testwebsite.utils.PropertiesManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -26,9 +28,15 @@ public class BrowserFactory {
                 firefoxOptions.addArguments("--start-maximized");
                 return new FirefoxDriver(firefoxOptions);
 
+            case "edge" :
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                return new EdgeDriver(edgeOptions);
+
              default :
 
-                 throw new IllegalStateException ("Unknown Driver Type");
+                throw new IllegalArgumentException("Unknown Driver Type !");
         }
 
 
